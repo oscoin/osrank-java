bd754c8233e9a35844baf6a0f38b9ab453225ce9
Ankur Delight <ankurdelight@gmail.com>
2019-11-06 12:47:58 -0800

    fixed bug that led to runtime errors unnecessarily

--- src/main/java/io/oscoin/algo/OsrankNaiveRun.java
+++ src/main/java/io/oscoin/algo/OsrankNaiveRun.java
@@ -60,7 +60,7 @@ public class OsrankNaiveRun {
             osrankResults.incrementVisitCount(currentNode.getNodeId());
 
             // If we get to a node that has no outbound edges, we end the walk
-            if (node.getConnectedNodeCount() == 0) break;
+            if (currentNode.getConnectedNodeCount() == 0) break;
 
             // Get the correct damping factor based on the node type
             double dampingFactor;
